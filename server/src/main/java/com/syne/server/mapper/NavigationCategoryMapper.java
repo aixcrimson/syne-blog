298c51e777aa9dc4a5a653cb9cff3fdb34cb15aa
GotohHitori <hitori150221@outlook.com>
2025-12-14 17:18:05 +0800

    feat: 开发导航站点管理CRUD接口

---
+++ server/src/main/java/com/syne/server/mapper/NavigationCategoryMapper.java
@@ -0,0 +1,18 @@
+package com.syne.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.syne.server.entity.NavigationCategory;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 导航分类Mapper接口
+ */
+@Mapper
+public interface NavigationCategoryMapper extends BaseMapper<NavigationCategory> {
+
+    /**
+     * 根据分类ID查询是否存在相关的导航站点
+     */
+    int countSitesByCategoryId(@Param("categoryId") Long categoryId);
+}
